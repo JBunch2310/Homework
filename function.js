5032a9c2ddc765c7c4de04b23c2ea40d3368c0bf
Jamie Bunch <jamiebunch@Jamies-MacBook-Air.local>
2022-01-19 14:35:36 -0500

    first git commit

---
+++ function.js
@@ -0,0 +1,7 @@
+function addition (a,b){
+    return a+b
+}
+const herAge =30
+const hisAge =31
+const totalAge =addition(herAge,hisAge)
+console.log('The total of Danielle and Jeremiahs ager are', totalAge)
---
+++ variable.js
@@ -0,0 +1,9 @@
+const country = 'USA'
+const continent = 'North America'
+const population = '331 population in millions'
+console.log('stats', {country, continent, population,})
+
+let firstName = 'Jamie'
+let lastName = 'Bunch'
+const myName = 'Jamie Bunch'
+console.log('Who am I?', lastName, myName,)
