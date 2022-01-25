2b8f162d90b3fff6e77ba5b5f94baf260394ac6d
Jamie Bunch <JamieBunch@ymail.com>
2022-01-25 16:30:59 -0500

    swecc2022_jamie

---
+++ swecc2022_jamie.sql
@@ -0,0 +1,58 @@
+USE Ecommerce;
+
+CREATE TABLE Customers_jamie (
+    CustomerId INT NOT NULL AUTO_INCREMENT,
+    FirstName TEXT,
+    LastName TEXT,
+    Email TEXT,
+    PhoneNumber TEXT,
+    PRIMARY KEY (CustomerId)
+);
+
+CREATE TABLE Address_jamie (
+    AddressId INT NOT NULL AUTO_INCREMENT,
+    StreetAddress VARCHAR(500),
+    City TEXT,
+    State TEXT,
+    PostalCode TEXT,
+    CustomerId INT,
+    PRIMARY KEY (AddressId),
+    FOREIGN KEY (CustomerId)
+        REFERENCES Customers_jamie (CustomerId)
+);
+
+CREATE TABLE Product_jamie (
+    ProductId INT NOT NULL AUTO_INCREMENT,
+    UnitPrice DECIMAL(12 , 2 ),
+    Quainity INT,
+    Description TEXT,
+    ProductName TEXT,
+    Location TEXT,
+    CustomerId INT,
+    PRIMARY KEY (ProductId),
+    FOREIGN KEY (CustomerId)
+        REFERENCES Customers_jamie (CustomerId)
+);
+
+CREATE TABLE Payments_jamie (
+    PaymentId INT AUTO_INCREMENT,
+    CreditCardNumber VARCHAR(50),
+    PaymentStatus TEXT,
+    OrderId TEXT,
+    CustomerId INT,
+    PRIMARY KEY (PaymentId),
+    FOREIGN KEY (CustomerId)
+        REFERENCES Customers_jamie (CustomerId)
+);
+
+CREATE TABLE Orders_jamie (
+    OrdersId INT AUTO_INCREMENT,
+    TotalAmount DECIMAL(12 , 2 ),
+    DeliveryDate DATETIME,
+    CustomerId INT,
+    PRIMARY KEY (OrdersId),
+    FOREIGN KEY (CustomerId)
+        REFERENCES Customers_jamie (CustomerId),
+    FOREIGN KEY (ProductId)
+        REFERENCES Products_jamie (PaymentId)
+);
